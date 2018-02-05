a7103dba13f48cdc30b9a84cef21185a0fcf1697
Danil Pasko <danilpasko@gmail.com>
2018-02-06 00:23:17 +0200

    some fixe name of class and add some comments in js file

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
-// filter of products on catalog page
+// filter 
 $(window).on('load', function() {
-	$(".filter-point").on('click', function() {
+	$(".filter-point").on('click', function() { //click on one of filter parameter
 		
-		$(".filter-point").removeClass('active');
-		$(this).addClass('active');
+		$(".filter-point").removeClass('active'); // delete class active from all filter parameters
+		$(this).addClass('active'); // and add class active for actual filter parameter
 
-		var block = $(".block");
+		var block = $(".block"); 
 		var actualFilter = $(this).attr('filterParameter');
 
-		block.addClass('hide-product');
+		block.addClass('hide-block'); // hide all blocks
 
- 		if (actualFilter === 'all') {
- 			block.removeClass('hide-product');
- 		}else {
- 			for (var i = 0; i < block.length; i++) {
- 				if ($(block[i]).hasClass(actualFilter)) {
- 					$(block[i]).removeClass('hide-product');
+ 		if (actualFilter === 'all') { //check what kind of filter parameter chose
+ 			block.removeClass('hide-block'); //If filter parameter 'ALL', remove class 'Hide-block' from all blocks, and can see all of them on page
+ 		}else { // if clicked other filter parameter
+ 			for (var i = 0; i < block.length; i++) { 
+ 				if ($(block[i]).hasClass(actualFilter)) { // I find from ALL blocks, what has a Class name like attribute of actual filte parameter
+ 					$(block[i]).removeClass('hide-block'); // And if find something , remove from it class Hide-block
  				};
  			};
  		};	
