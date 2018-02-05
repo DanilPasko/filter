26b25c53ca3ad9f98ab5115d53b6c7b45357cf4e
Danil Pasko <danilpasko@gmail.com>
2018-02-06 00:36:42 +0200

    add comments for all files

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 
 // filter 
 $(window).on('load', function() {
-	$(".filter-point").on('click', function() { //click on one of filter parameter
+	$(".filter-point").on('click', function() { //chose one of filter parameter
 		
-		$(".filter-point").removeClass('active'); // delete class active from all filter parameters
-		$(this).addClass('active'); // and add class active for actual filter parameter
+		$(".filter-point").removeClass('active'); // delete class 'active' from all filter parameters
+		$(this).addClass('active'); // and add class 'active' for actual filter parameter - it's for some styles for actual filter parameter
 
-		var block = $(".block"); 
-		var actualFilter = $(this).attr('filterParameter');
+		var block = $(".block");  // blocks for filtration
+		var actualFilter = $(this).attr('filterParameter'); // find attribute name of actual filter parameter
 
-		block.addClass('hide-block'); // hide all blocks
+		block.addClass('hide-block'); // hide all blocks 
 
- 		if (actualFilter === 'all') { //check what kind of filter parameter chose
- 			block.removeClass('hide-block'); //If filter parameter 'ALL', remove class 'Hide-block' from all blocks, and can see all of them on page
- 		}else { // if clicked other filter parameter
+//check what kind of filter parameter chose
+ 		if (actualFilter === 'all') { //If filter parameter 'ALL'
+ 			block.removeClass('hide-block'); //show all blocks 
+ 		}else { // if chose other filter parameter
  			for (var i = 0; i < block.length; i++) { 
- 				if ($(block[i]).hasClass(actualFilter)) { // I find from ALL blocks, what has a Class name like attribute of actual filte parameter
- 					$(block[i]).removeClass('hide-block'); // And if find something , remove from it class Hide-block
+ 				if ($(block[i]).hasClass(actualFilter)) { // find from all blocks, only those that have a class like attribute of actual filter parameter
+ 					$(block[i]).removeClass('hide-block'); // And if find something, show them 
  				};
  			};
  		};	
